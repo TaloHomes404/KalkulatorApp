80ab8275f6479402c42d6a90fb87edf8daf6b05f
Bartek <bartlomiej00wilk@gmail.com>
2023-04-07 17:11:53 +0200

    Tworzenie UI kalkulatora

---
+++ app/src/main/java/wolf/north/kalkulatorapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package wolf.north.kalkulatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun wcisniecieLiczby (view: View){
+        Toast.makeText(this, "Wybrano liczbe", Toast.LENGTH_LONG).show()
+    }
+}
