ef3f31a8dcac0d77a6abe9eaa6b6f7064cf57006
Bartek <bartlomiej00wilk@gmail.com>
2023-04-07 18:27:33 +0200

    funkcja - wprowadzajaca tekst z przycisku na ekran ui - ustawienie wszystkiego na gotowe

--- app/src/main/java/wolf/north/kalkulatorapp/MainActivity.kt
+++ app/src/main/java/wolf/north/kalkulatorapp/MainActivity.kt
@@ -3,14 +3,27 @@ package wolf.north.kalkulatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+        //DEKLARACJA ELEMENTÓW UI
+        private var textInput: TextView? = null
+        //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //INICJALIZACJA ZADEKLAROWANYCH ELEMENTÓW UI
+        textInput = findViewById(R.id.textInput)
+        //
+
     }
     fun wcisniecieLiczby (view: View){
-        Toast.makeText(this, "Wybrano liczbe", Toast.LENGTH_LONG).show()
+
+        //BUTTON NALEŻY DO GRUPY VIEW, DEKLARUJEMY GO JAKO WARUNEK (JEŻELI VIEW TO PRZYCISK TO POBIERAMY JEGO TEKST)
+        textInput?.append((view as Button).text)
     }
 }
