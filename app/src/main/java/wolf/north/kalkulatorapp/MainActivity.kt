676fa87799bc1852ed61b34a8d29ea31413aa815
Bartek <bartlomiej00wilk@gmail.com>
2023-04-12 16:26:05 +0200

    FUNKCJE - WCIŚNIĘCIE OPERATORA POWODUJE DODANIE GO ALE NIE MOŻNA JUŻ DODAWAĆ WIĘCEJ NIŻ JEDNĄ OPERACJĘ MOŻEMY UŻYWAĆ WARTOŚCI UJEMNE
    WCIŚNIĘCIE OPERATORA POWODUJE SPRAWDZENIE WARUNKÓW CZY MAMY LICZBĘ W POLU, CZY OPERATOR JUŻ NIE WYSTĘPUJE ITP

--- app/src/main/java/wolf/north/kalkulatorapp/MainActivity.kt
+++ app/src/main/java/wolf/north/kalkulatorapp/MainActivity.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
 
         //DEKLARACJA ELEMENTÓW UI
         private var textInput: TextView? = null
+
         //
+        //FLAGA - SPRAWDZA CZY W POLU TEKSTOWYM ZNAJDUJE SIĘ NUMER NA KOŃCU
+        var lastIsNumber : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +28,9 @@ class MainActivity : AppCompatActivity() {
     }
     fun wcisniecieLiczby (view: View){
         //BUTTON NALEŻY DO GRUPY VIEW, DEKLARUJEMY GO JAKO WARUNEK (JEŻELI VIEW TO PRZYCISK TO POBIERAMY JEGO TEKST)
+        //FLAGA lastIsNumber ZMIENIA NA WARTOŚĆ TRUE PONIEWAŻ WCISKAMY LICZBE- WIĘC TAKA ZNAJDUJE SIĘ NA KOŃCU TEKSTU
         textInput?.append((view as Button).text)
+        lastIsNumber = true
     }
 
     fun wcisniecieCLR (view: View){
@@ -33,16 +40,42 @@ class MainActivity : AppCompatActivity() {
 
     fun dodajCzescDziesietna(view: View){
         //FUNKCJONALNOŚĆ PRZYCISKU "." - WCIŚNIĘCIE DODAJE DO LICZBY CZĘŚĆ DZIESIĘTNĄ
+        //lastIsNumber - FLAGA ZMNIENIA WARTOŚĆ NA FAŁSZ, PONIEWAŻ NA KOŃCU MAMY KROPKĘ, ALBO PUSTKI
 
         if(textInput?.text.toString().endsWith('.')){
+            lastIsNumber = false
             return
         }else if(textInput?.text.toString().contains('.')){
             return
         }else if(textInput?.text.toString().isBlank()){
+            lastIsNumber = false
             return
         }else{
             textInput?.append(".")
+            lastIsNumber = false
         }
     }
 
+    private fun czyWybranyOperator(poleTekstowe : String) : Boolean {
+        //FUNKCJA KTÓRA WERYFIKUJE CZY TEKST ZAWIERA JUŻ ZNAK OPERATORA (DODAWANIE,ODEJMOWANIE,DZIELENIE,MNOŻENIE)
+        //PIERWSZY WARUNEK SPRAWDZA CZY NIE ZACZYNA SIĘ OD MINUSA (LICZBY UJEMNE) = WTEDY IGNORUJE TEN WARUNEK BO CHCEMY OPEROWAĆ RÓWNIEŻ NA WARTOŚCIACH UJEMNYCH
+        return if(poleTekstowe.startsWith('-')){
+            false
+        }else{
+            poleTekstowe.contains('/')
+                    || poleTekstowe.contains('*')
+                    || poleTekstowe.contains('+')
+                    || poleTekstowe.contains('-')
+        }
+    }
+
+    fun wcisniecieOperatora(view: View){
+
+        //FUNCKJONALNOŚĆ KTÓRA DODAJE DO TEKSTU OPERATOR MATEMATYCZNY - Z WARUNKAMI KTÓRE SPRAWIAJĄ
+        //ŻE TYLKO JEDNA OPERACJA MOŻE BYĆ WYKONANA W JEDNYM CZASIE
+        if (lastIsNumber && !czyWybranyOperator(textInput?.text.toString())){
+            textInput?.append((view as Button).text)
+            lastIsNumber = false
+        }
+    }
 }
